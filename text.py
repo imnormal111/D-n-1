80b7af6a260406c7e1accff4b4e10f2c2e9fa6e4
imnormal111 <slgamingsamandlouis@gmail.com>
2026-08-07 19:25:38 +0700

    Update text.py

--- text.py
+++ text.py
@@ -1,2 +1,17 @@
 print("Hello, World!")
-print("Bạn đang làm gì vậy?")
+print("Bạn đang làm gì vậy?")#c = string.digits
+#tat_ca = a + b + c + d
+#print(tat_ca)
+#f = ["P", "Y", "T", "H", "O", "N"]
+#e = "".join(f)
+#print(e)
+#Bài 3
+#ask = int(input("Nhập độ dài mật khẩu:"))
+#mk = "".join(random.choices(tat_ca, k=ask))
+#print(mk)
+a = []
+for i in range(5):
+    s = random.randint(0, 9)
+    a.append(s)
+    
+print(a)
