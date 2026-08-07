a0f3a5c81d12d0bc27f71d880b558dba35731776
imnormal111 <slgamingsamandlouis@gmail.com>
2026-08-07 19:27:52 +0700

    Update text.py

--- text.py
+++ text.py
@@ -1,7 +1,13 @@
-print("Hello, World!")
-print("Bạn đang làm gì vậy?")#c = string.digits
+import string
+import random
+#bài 1
+#a = string.ascii_lowercase
+#b = string.ascii_uppercase
+#c = string.digits
+#d = string.punctuation
 #tat_ca = a + b + c + d
 #print(tat_ca)
+#bài 2
 #f = ["P", "Y", "T", "H", "O", "N"]
 #e = "".join(f)
 #print(e)
@@ -9,9 +15,9 @@ print("Bạn đang làm gì vậy?")#c = string.digits
 #ask = int(input("Nhập độ dài mật khẩu:"))
 #mk = "".join(random.choices(tat_ca, k=ask))
 #print(mk)
-a = []
-for i in range(5):
-    s = random.randint(0, 9)
-    a.append(s)
+#ss = []
+#for i in range(5):
+    #s = random.randint(0, 9)
+   # ss.append(s)
     
-print(a)
+#print(ss)
