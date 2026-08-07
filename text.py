60df23722eecef48ba63b7899252b00d885e3817
imnormal111 <slgamingsamandlouis@gmail.com>
2026-08-07 19:41:47 +0700

    Update text.py

--- text.py
+++ text.py
@@ -15,9 +15,33 @@ import random
 #ask = int(input("Nhập độ dài mật khẩu:"))
 #mk = "".join(random.choices(tat_ca, k=ask))
 #print(mk)
+#Bài 4
 #ss = []
 #for i in range(5):
     #s = random.randint(0, 9)
    # ss.append(s)
     
 #print(ss)
+#Bài 5
+#a = string.ascii_lowercase
+#b = string.ascii_uppercase
+#c = string.digits
+#d = string.punctuation
+#all  = a + b + c + d
+#do_dai = int(input("Nhập độ dài mật khẩu:"))
+#if do_dai <4:
+ #   print("Độ dài mật khẩu phải dài hơn để chứa đủ 4 nhóm ký tự")
+#else:
+   # mat_khau = [random.choice(a) + random.choice(b) + random.choice(c) + random.choice(d)]
+   # mat_khau += random.choices(all, k=do_dai - 4)
+   # random.shuffle(mat_khau)
+   # ketqua = "".join(mat_khau)
+    #  print("Kết quả mật khẩu:", ketqua)
+#Bài 6
+do_dai = int(input("Nhập: "))
+
+if do_dai < 4:
+    print("Kết quả:")
+    print("Cần ít nhất 4 ký tự!")
+else:
+    print("Độ dài hợp lệ")
