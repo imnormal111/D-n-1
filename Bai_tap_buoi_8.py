9de4322371d02475d215204f390dacd4822bece6
imnormal111 <slgamingsamandlouis@gmail.com>
2026-08-14 19:21:42 +0700

    Update Bai_tap_buoi_8.py

--- Bai_tap_buoi_8.py
+++ Bai_tap_buoi_8.py
@@ -38,3 +38,52 @@ import random, string
 #for i in range(1,4):
    # b = "".join(random.choices(a, k=10))
    # print(f"Mật khẩu {i}: {b}")
+#Bài 8
+#ky_tu_nham = "I1O0l"
+#kho_goc = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+#kho_moi = ""
+#for i in kho_goc:
+#    if i not in ky_tu_nham:
+#        kho_moi += i
+
+#do_dai = int(input("Nhập độ dài mật khẩu:"))
+#if do_dai < 4:
+ #   print("Độ dài mật khẩu phải dài hơn để chứa đủ 4 nhóm ký tự")
+#else:
+ #   mat_khau = random.choices(kho_moi, k=do_dai)
+  #  ketqua = "".join(mat_khau)
+#print(ketqua)
+#Bài 9
+#d = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+#do_dai = int(input("Nhập độ dài mật khẩu:"))
+#if do_dai < 4:
+#    print("Độ dài mật khẩu phải dài hơn để chứa đủ 4 nhóm ký tự")
+#else:
+ #   mat_khau = random.choices(d, k=do_dai)
+  #  a = "".join(mat_khau)
+
+#if do_dai <8:
+ #   print("Mật khẩu yếu")
+#elif do_dai <=11:
+ #   print("Mật khẩu trung bình")
+#else:
+ #   print("Mật khẩu mạnh")
+#print("Mật khẩu:", a)
+#Bài 10
+a = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+do_dai = int(input("Nhập độ dài mật khẩu:"))
+if do_dai < 4:
+    print("Độ dài mật khẩu phải dài hơn để chứa đủ 4 nhóm ký tự")
+else:
+    mat_khau = [random.choice(string.ascii_lowercase), random.choice(string.ascii_uppercase), random.choice(string.digits), random.choice(string.punctuation)]
+    mat_khau += random.choices(a, k=do_dai - 4)
+    mk = "".join(mat_khau)
+    random.shuffle(list(mk))
+    
+if do_dai <8:
+    print("Mật khẩu yếu")
+elif do_dai <=11:
+    print("Mật khẩu trung bình")
+else:
+    print("Mật khẩu mạnh")
+print("Mật khẩu:", mk)
