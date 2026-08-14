d5c5e5edd4e7b99d137dd4e38379eba0fdbcf95c
imnormal111 <slgamingsamandlouis@gmail.com>
2026-08-14 19:03:37 +0700

    Update Bai_tap_buoi_8.py

--- Bai_tap_buoi_8.py
+++ Bai_tap_buoi_8.py
@@ -15,3 +15,26 @@ import random, string
 #ask = int(input("Nhập độ dài mật khẩu:"))
 #mk = "".join(random.choices(tat_ca, k=ask))
 #print(mk)
+#Bài 4
+# s=[]
+# for i in range(5):
+#     s = random.randint(0, 9)
+#     s.append(s)
+#print(s)
+#Bài 5&6
+#a = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+#do_dai = int(input("Nhập độ dài mật khẩu:"))
+#if do_dai < 4:
+  #  print("Độ dài mật khẩu phải dài hơn để chứa đủ 4 nhóm ký tự")  
+#else:
+    #mat_khau = [random.choice(string.ascii_lowercase), random.choice(string.ascii_uppercase), random.choice(string.digits), random.choice(string.punctuation)]
+   # mat_khau += random.choices(a, k=do_dai - 4)
+   # random.shuffle(mat_khau)
+    #ketqua = "".join(mat_khau)
+    #print("Kết quả mật khẩu:", ketqua)
+#Bài 7
+#a = string.ascii_lowercase+string.ascii_uppercase+string.digits+string.punctuation
+#print("Kết quả(ví dụ):")
+#for i in range(1,4):
+   # b = "".join(random.choices(a, k=10))
+   # print(f"Mật khẩu {i}: {b}")
