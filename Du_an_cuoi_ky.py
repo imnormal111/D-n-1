f1e0c7ff9ec1aea9c08421bf3d11bcb1bb08d235
imnormal111 <slgamingsamandlouis@gmail.com>
2026-08-21 19:51:04 +0700

    Update Du_an_cuoi_ky.py

--- Du_an_cuoi_ky.py
+++ Du_an_cuoi_ky.py
@@ -2,9 +2,9 @@
 import random, string
 phuong_tien = input("Nhập phương tiện đi lại (xe máy, ô tô, xe buýt, tàu hỏa, máy bay): ")
 quang_duong = float(input("Nhập quãng đường(km): "))
-loai_xang = input("Nhập loại xăng (A95, A92, E5): ")
 muc_tieu_thu = float(input("Nhập mức tiêu thụ nhiên liệu (lít/100km): "))
 if phuong_tien.lower() == "xe máy":
+    loai_xang = input("Nhập loại xăng (A95, A92, E5): ")
     if loai_xang.upper() == "A95":
         gia_xang = 25000
     elif loai_xang.upper() == "A92": #Loại xăng
@@ -19,6 +19,7 @@ if phuong_tien.lower() == "xe máy":
     ma_chuyen_di = "".join(random.choices(string.ascii_uppercase + string.digits, k=6)) #TẠO MÃ CHUYẾN ĐI
     print(f"Mã chuyến đi: {ma_chuyen_di}")
 elif phuong_tien.lower() == "ô tô":
+    loai_xang = input("Nhập loại xăng (A95, A92, E5): ")
     if loai_xang.upper() == "A95":
         gia_xang = 25000
     elif loai_xang.upper() == "A92": #Loại xăng
@@ -32,6 +33,22 @@ elif phuong_tien.lower() == "ô tô":
     print(f"Chi phí chuyến đi bằng {phuong_tien} là: {chi_phi} VND")
     ma_chuyen_di = "".join(random.choices(string.ascii_uppercase + string.digits, k=6)) #TẠO MÃ CHUYẾN ĐI
     print(f"Mã chuyến đi: {ma_chuyen_di}")
+elif phuong_tien.lower() == "xe buýt":
+    loai_xang = input("Nhập loại xăng (A95, A92, E5): ")
+    if loai_xang.upper() == "A95":
+        gia_xang = 25000
+    elif loai_xang.upper() == "A92": #Loại xăng
+        gia_xang = 23000
+    elif loai_xang.upper() == "E5":
+        gia_xang = 22000
+    else:
+        print("Loại xăng không hợp lệ")
+        exit()
+    chi_phi = quang_duong * 5000 #Giả sử giá vé xe buýt là 5000 VND/km
+    print(f"Chi phí chuyến đi bằng {phuong_tien} là: {chi_phi} VND")
+    ma_chuyen_di = "".join(random.choices(string.ascii_uppercase + string.digits, k=6)) #TẠO MÃ CHUYẾN ĐI
+    print(f"Mã chuyến đi: {ma_chuyen_di}")
+    
     
     
     
